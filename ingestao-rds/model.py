64a795967b1acbf728d481579a718592467e29f8
heliton1986 <heliton.ds86@gmail.com>
2023-02-13 13:31:28 -0300

    new commit

--- ingestao-rds/model.py
+++ ingestao-rds/model.py
@@ -26,8 +26,7 @@ class Coins(Base):
     percent_change_7d = Column(Float)
    
     def start():
-        db_string = "postgresql://postgres:hghjghjgjj@server01.hgjhghjgjhgj.us-east-1.rds.amazonaws.com/coins"
-        db_string = "postgresql://postgres:xxxx@xxxxx.xxxxx.us-east-1.rds.amazonaws.com/coins"
+        db_string = "postgresql://postgres:*8xayZycAE5m@server01.csddqd4agbf7.us-east-1.rds.amazonaws.com/coins"
         engine = create_engine(db_string)
         Session = sessionmaker(bind=engine)
         session = Session()
